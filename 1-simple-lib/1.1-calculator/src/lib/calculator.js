030a2322d53195d07f335389246fd1343cfd86c2
theandersonn <theandersonn@gmail.com>
2022-06-29 12:01:10 -0300

    1.1: jest watch mode - observar alteracoes nos arquivos

--- 1-simple-lib/1.1-calculator/src/lib/calculator.js
+++ 1-simple-lib/1.1-calculator/src/lib/calculator.js
@@ -1,3 +1,10 @@
-module.exports.sum = (num1, num2) => {
+module.exports.sum = (param1, param2) => {
+  const num1 = parseInt(param1, 10); 
+  const num2 = parseInt(param2, 10); 
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    throw new Error('Please check ypur input');
+  }
+
   return num1 + num2;
 };
